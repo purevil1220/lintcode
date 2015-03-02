80abd449607f004696805f95d7776dac9251743b
purevil1220 <jjl1220@gmail.com>
2015-03-01 23:53:36 -0800

    better

--- JumpGameII.cpp
+++ JumpGameII.cpp
@@ -1,3 +1,35 @@
+// modified
+class Solution {
+public:
+    /**
+     * @param A: A list of lists of integers
+     * @return: An integer
+     */
+    int jump(vector<int> A) {
+        // wirte your code here
+        int n=A.size();
+        if(n<=1) return 0;
+        
+        int step=0;
+        int s=0;
+        int e=0;
+        while(s<=e) {
+            step++;
+            int reach=e;
+            for(int i=s; i<=e; i++) {
+                if(A[i]+i>=n-1) return step;
+                reach = max(reach, A[i]+i);
+            }
+            s=e+1;
+            e=reach;
+        }
+        
+        return -1;
+        
+    }
+};
+
+
 class Solution {
 public:
     /**
